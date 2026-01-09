b548c314e0be7a638fd4c8ad55e270f0212e1e0b
Mr.Xiao <xjlbibi0825@gmail.com>
2026-01-09 10:37:52 +0800

    修改famousQuote.jsx文件  添加投票和计数以及统计出最大投票数的数据

--- part1/src/famousQuote.jsx
+++ part1/src/famousQuote.jsx
@@ -12,18 +12,36 @@ const FamousQuote = () => {
         'The only way to go fast, is to go well.'
     ]
 
-    const [selected, setSelected] = useState(0)
+    const [selected, setSelected] = useState(Math.floor(Math.random() * anecdotes.length));
+    const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
 
+
+    const Button = ({ handleClick, text }) => (
+        <button onClick={handleClick}>
+            {text}
+        </button>
+    )
     const generateRandomQuote = () => {
         const randomIndex = Math.floor(Math.random() * anecdotes.length);
         setSelected(randomIndex);
+    }
+    
+    const voteQuote = (index) => {
+        const newVotes = [...votes];
+        newVotes[index] += 1;
+        setVotes(newVotes);
     }
 
     return (
         <div>
             <h3>Random Famous Quote:</h3>
             <p>{anecdotes[selected]}</p>
-            <button onClick={generateRandomQuote}>Random Quote</button>
+            <p>Has {votes[selected]} votes</p>
+            <Button handleClick={() => voteQuote(selected)} text="Vote" />
+            <Button handleClick={generateRandomQuote} text="Random Quote" />
+            <h3>Most Voted Quote:</h3>
+            <p>{anecdotes[votes.indexOf(Math.max(...votes))]}</p>
+            <p>it has {Math.max(...votes)} votes</p>
         </div>
     )
 }
