d3131dd1501913de1c00551616c7a85e0d7c4976
Mr.Xiao <xjlbibi0825@gmail.com>
2026-01-18 23:51:13 +0800

    添加part2-nodes-forms表单

--- part2-notes-frontend/src/App.jsx
+++ part2-notes-frontend/src/App.jsx
@@ -1,16 +1,39 @@
-import Note from './components/Note'
+import Note from "./components/Note";
+import { useState } from "react";
 
 const App = ({ notes }) => {
+  const [notesList, setNotesList] = useState(notes);
+  const [newNote, setNewNote] = useState("");
+
+  const processSubmission = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notesList.length + 1,
+    };
+    setNotesList(notesList.concat(noteObject));
+    setNewNote("");
+  };
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
+
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map((note) => (
+        {notesList.map((note) => (
           <Note key={note.id} note={note} />
         ))}
       </ul>
+      <form onSubmit={processSubmission}>
+        <input value={newNote} onChange={handleChange} />
+        <button type="submit">save</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
